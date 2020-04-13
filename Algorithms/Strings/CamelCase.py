ccee599478f4367409fddfb6143e098af06c3d73
Shaswata Das <shaswata0999@gmail.com>
2020-04-14 02:47:29 +0530

    Update CamelCase.py

--- Algorithms/Strings/CamelCase.py
+++ Algorithms/Strings/CamelCase.py
@@ -1,1 +1,23 @@
+import math
+import os
+import random
+import re
+import sys
 
+def camelcase(s):
+    count=1
+    for i in s:
+        if i.isupper():
+            count+=1
+    return count
+            
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
