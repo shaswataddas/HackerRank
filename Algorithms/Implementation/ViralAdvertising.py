469a6c46c5bb6a67c3d88df2095f9339a871e943
Shaswata Das <60272712+shaswataddas@users.noreply.github.com>
2020-04-13 21:36:50 +0530

    Update ViralAdvertising.py

--- Algorithms/Implementation/ViralAdvertising.py
+++ Algorithms/Implementation/ViralAdvertising.py
@@ -1,1 +1,26 @@
+import math
+import os
+import random
+import re
+import sys
 
+def viralAdvertising(n):
+    shared=5
+    mysum=0
+    while n>0:
+        like_no=math.floor(shared/2)
+        mysum+=like_no
+        shared=like_no*3
+        n-=1
+    return mysum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
