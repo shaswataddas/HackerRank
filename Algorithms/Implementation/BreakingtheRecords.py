076c225694c2d9eb04abc6705f2b70c74fe635de
Shaswata Das <60272712+shaswataddas@users.noreply.github.com>
2020-04-13 21:21:10 +0530

    Update BreakingtheRecords.py

--- Algorithms/Implementation/BreakingtheRecords.py
+++ Algorithms/Implementation/BreakingtheRecords.py
@@ -1,1 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
 
+
+def breakingRecords(scores):
+    mini=scores[0]
+    maxi=scores[0]
+    p=0
+    q=0
+    for i in scores:
+        if i<mini:
+            mini=i
+            p+=1   
+        if i>maxi:
+            maxi=i
+            q+=1
+    return(q,p)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
