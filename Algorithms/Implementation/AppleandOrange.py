5aa05f399f7d2a95504b5db91d78afa1f330820d
Shaswata Das <60272712+shaswataddas@users.noreply.github.com>
2020-04-13 21:16:59 +0530

    Update AppleandOrange.py

--- Algorithms/Implementation/AppleandOrange.py
+++ Algorithms/Implementation/AppleandOrange.py
@@ -1,1 +1,51 @@
+import math
+import os
+import random
+import re
+import sys
 
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    aplist=[]
+    orlist=[]
+    resultapp=0
+    resultor=0
+
+    for i in apples:
+        aplist.append(a+i)
+    for j in oranges:
+        orlist.append(b+j)
+
+    for i in aplist:
+        if i>=s and i<=t:
+            resultapp+=1
+    for j in orlist:
+        if j>=s and j<=t:
+            resultor+=1
+    print(resultapp)
+    print(resultor)
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
