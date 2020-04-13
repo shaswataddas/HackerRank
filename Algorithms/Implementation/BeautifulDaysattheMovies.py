502edfb141fb4a719cdd4bb980215c269bf978ab
Shaswata Das <60272712+shaswataddas@users.noreply.github.com>
2020-04-13 21:34:48 +0530

    Update BeautifulDaysattheMovies.py

--- Algorithms/Implementation/BeautifulDaysattheMovies.py
+++ Algorithms/Implementation/BeautifulDaysattheMovies.py
@@ -1,1 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
 
+def beautifulDays(i, j, k):
+    temp=0
+    while i<=j:
+        rev=str(i)[::-1]
+        diff=i-int(rev)
+        if diff%k==0:
+            temp+=1
+        i+=1
+    return temp
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ijk = input().split()
+
+    i = int(ijk[0])
+
+    j = int(ijk[1])
+
+    k = int(ijk[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
