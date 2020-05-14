d1849e166a70e52eb9b614488a9d15353cd8ca64
Shaswata Das <shaswata0999@gmail.com>
2020-05-15 02:34:00 +0530

    Update Missing_Numbers.py

--- Algorithms/Search/Missing_Numbers.py
+++ Algorithms/Search/Missing_Numbers.py
@@ -1,1 +1,33 @@
 
+import math
+import os
+import random
+import re
+import sys
+
+
+def missingNumbers(arr, brr):
+    for i in arr:
+        brr.remove(i)
+    brr=set(brr)
+    brr= list(brr)
+    brr.sort()
+    return brr
+     
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    m = int(input())
+
+    brr = list(map(int, input().rstrip().split()))
+
+    result = missingNumbers(arr, brr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
